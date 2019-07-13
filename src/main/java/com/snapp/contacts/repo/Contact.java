80a8d2f426ea31251d1e2ee01dc78fa1656dbfa6
nooshin <nooshin.hajibandeh@gmail.com>
2019-07-13 10:26:34 +0430

    change serialVersionId

--- src/main/java/com/snapp/contacts/repo/Contact.java
+++ src/main/java/com/snapp/contacts/repo/Contact.java
@@ -7,8 +7,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "Contact")
 public class Contact implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+    private static final long serialVersionUID = -2779924478272408505L;
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
