acfcbd40659c57ee85171e3eb7b809ed012bb141
nooshin <nooshin.hajibandeh@gmail.com>
2019-07-13 10:07:50 +0430

    add setter method

--- src/main/java/com/snapp/contacts/exception/ExceptionResponse.java
+++ src/main/java/com/snapp/contacts/exception/ExceptionResponse.java
@@ -14,6 +14,18 @@ public class ExceptionResponse {
         this.details = details;
     }
 
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
     public Date getTimestamp() {
         return timestamp;
     }
